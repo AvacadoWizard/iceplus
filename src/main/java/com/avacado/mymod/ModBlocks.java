ef1285385e099b84fd7d6de706d3a378cb905d82
Aleks Lillis <alekslillis1@gmail.com>
2025-06-04 12:54:48 -0400

    brooooo

---
+++ src/main/java/com/avacado/mymod/ModBlocks.java
@@ -0,0 +1,44 @@
+package com.avacado.mymod;
+
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.RegistryObject;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModBlocks {
+    public static final String MODID = "iceplus";
+
+    // very blue ice block in item and block :)
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, VeryBlueIce.MODID);
+    public static final RegistryObject<Block> VERY_BLUE_ICE  = BLOCKS.register("very_blue_ice", () -> new VeryBlueIce());
+
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, VeryBlueIce.MODID);
+    public static final RegistryObject<BlockItem> VERY_BLUE_ICE_ITEM = ITEMS.register("very_blue_ice", () -> new BlockItem(VERY_BLUE_ICE.get(), new Item.Properties()));
+    
+    // osrs put that john in creative
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+        ITEMS.register(eventBus);
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+
+    public static void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS)
+            event.accept(VERY_BLUE_ICE_ITEM);
+    }
+
+    
+}
